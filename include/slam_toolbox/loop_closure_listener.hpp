4a6969aa3fea145c3a018433e02ae208f7ee5b37
hyunsehyun <99864704+hyunsehyun@users.noreply.github.com>
2025-10-01 12:34:08 +0900

    change variable name

--- include/slam_toolbox/loop_closure_listener.hpp
+++ include/slam_toolbox/loop_closure_listener.hpp
@@ -17,16 +17,16 @@ namespace slam_toolbox {
 class LoopClosureListener final : public karto::MapperLoopClosureListener {
 public:
   LoopClosureListener(
-      std::weak_ptr<rclcpp_lifecycle::LifecyclePublisher<slam_toolbox::msg::LoopClosureEvent>> pub,
+      std::weak_ptr<rclcpp_lifecycle::LifecyclePublisher<slam_toolbox::msg::LoopClosureEvent>> loop_closure_event_pub,
       std::weak_ptr<rclcpp::Clock> clock,
-      std::function<void()> republish_graph);
+      std::function<void()> loop_closure_publish_graph);
 
   void EndLoopClosure(const std::string & rInfo) override;
 
 private:
-  std::weak_ptr<rclcpp_lifecycle::LifecyclePublisher<slam_toolbox::msg::LoopClosureEvent>> pub_;
+  std::weak_ptr<rclcpp_lifecycle::LifecyclePublisher<slam_toolbox::msg::LoopClosureEvent>> loop_closure_event_pub_;
   std::weak_ptr<rclcpp::Clock> clock_;
-  std::function<void()> republish_graph_;
+  std::function<void()> loop_closure_publish_graph_;
 };
 
 } // namespace slam_toolbox
