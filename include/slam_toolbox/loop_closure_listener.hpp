13f1f519d4df2230d02d863a925cc13700e70a2c
hyunsehyun <99864704+hyunsehyun@users.noreply.github.com>
2025-10-01 16:24:26 +0900

    Update LoopClosureListener constructor parameters

--- include/slam_toolbox/loop_closure_listener.hpp
+++ include/slam_toolbox/loop_closure_listener.hpp
@@ -19,14 +19,14 @@ public:
   LoopClosureListener(
       std::weak_ptr<rclcpp_lifecycle::LifecyclePublisher<slam_toolbox::msg::LoopClosureEvent>> loop_closure_event_pub,
       std::weak_ptr<rclcpp::Clock> clock,
-      std::function<void()> loop_closure_publish_graph);
+      std::function<void()> loop_closure_callback);
 
   void EndLoopClosure(const std::string & rInfo) override;
 
 private:
   std::weak_ptr<rclcpp_lifecycle::LifecyclePublisher<slam_toolbox::msg::LoopClosureEvent>> loop_closure_event_pub_;
   std::weak_ptr<rclcpp::Clock> clock_;
-  std::function<void()> loop_closure_publish_graph_;
+  std::function<void()> loop_closure_callback_;
 };
 
 } // namespace slam_toolbox
